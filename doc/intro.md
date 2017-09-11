5b84408738b3f90df56b5248826758fd75a6516f
Daniel Warren <tryandspeak@aries.pantheon.net>
2017-09-10 18:04:33 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bowly
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/bowly/core_test.clj
@@ -0,0 +1,7 @@
+(ns bowly.core-test
+  (:require [clojure.test :refer :all]
+            [bowly.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
