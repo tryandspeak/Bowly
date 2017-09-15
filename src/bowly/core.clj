a3220e35a55c4c78447a20c900538f65789875c0
Daniel Warren <tryandspeak@aries.pantheon.net>
2017-09-14 18:55:41 -0600

    Connect and echo. Moved to Clojure

--- src/bowly/core.clj
+++ src/bowly/core.clj
@@ -1,7 +1,37 @@
 (ns bowly.core
   (:gen-class))
 
+(require '[irclj.core :as irc])
+(require '[clojure.string  :as string])
+
+(defn echo [s] s)
+
+(def commands {
+               ";echo" echo
+               })
+
+(defn trim-command [s]
+  (string/join " " (rest (string/split s #"\s"))))
+
+(defn append-sender [msg s]
+  (string/join " " [(str (:nick msg) ":") s]))
+
+(defn parse-command [s]
+   (first (string/split s #"\s")))
+
+(defn privmsg [irc msg & s]
+  "General reply to what's going on. Execute commands."
+  (let [command (parse-command (:text msg))]
+     (irc/reply irc
+               msg
+               ((get commands command (fn [& args]))
+               (append-sender msg (trim-command (:text msg)))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Connect and start listening"
   [& args]
-  (println "Hello, World!"))
+  ((def connection (irc/connect "irc.freenode.net" 6667 "Bowly"
+                                :callbacks {
+                                            :privmsg privmsg
+                                            }))
+   (irc/join connection "#hahasame")))
